dd9d31c8b2ec2e1a91babfd06c849168930e50be
ndumthiy021 <ndumthiy021@student.wethinkcode.co.za>
2022-10-09 17:29:41 +0200

    winners list sorted

--- js/spa-fpl.js
+++ js/spa-fpl.js
@@ -3,17 +3,18 @@ window.addEventListener("load", () => {
       method: 'GET',                                                          
   };
 
-  const word = 'code';
-
   fetch(`https://fantasy.premierleague.com/api/leagues-classic/1982813/standings/?page_standings=1`, options)   
       .then(response => response.json())                                      
       .then(data => {
-          const div = document.createElement('div')
-          
+          const div = document.createElement('div');
+          const div2 = document.createElement('div');
           div.appendChild(createElementWithContent('h2', data.league.name));
           div.appendChild(createElementWithContent('h3', "GAMEWEEK RESULTS"));
-          div.appendChild(createListWithContent('ol', data.standings.results));
-          document.getElementById('app').replaceChildren(div);
+          div.appendChild(createListWithContent('ol', data.standings.results,'event_total'));
+          div2.appendChild(createElementWithContent('h3', "OVERALL RESULTS"));
+          div2.appendChild(createListWithContent('ol', data.standings.results, 'total'));
+          document.getElementById('gameweek').replaceChildren(div);
+          document.getElementById('overall').replaceChildren(div2);
       });    
         
       function createElementWithContent(type, content) {                                                      
@@ -22,17 +23,27 @@ window.addEventListener("load", () => {
           return el;
       }
   
-      function createListWithContent(type, list) {                                                  
+      function createListWithContent(type, list, description) {                                                  
           const el = document.createElement(type);
-          const sortedList = list.sort(compareGameweekScores)
-          sortedList.forEach(item => el.appendChild(createElementWithContent('li', item['entry_name'] + " | " + 
-          item['event_total'])));
+          let sortedList;
+          if (description == 'event_total'){
+            sortedList = list.sort(compareGameweekScores);
+          }
+          else{
+            sortedList = list.sort(compareOverallScores);
+          }
+          sortedList.forEach(item => el.appendChild(createElementWithContent('li', item['player_name'] + " | " + 
+          item[description])));
           return el;
       }
 
       function compareGameweekScores(a, b) {
         return b.event_total - a.event_total;
-      }    
+      } 
+      
+      function compareOverallScores(a, b) {
+        return b.total - a.total;
+      }  
 });
 
 
